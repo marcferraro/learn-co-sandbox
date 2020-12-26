9fcd2540a1f5030b4e5bb035855db2808265960c
Marc Ferraro <marc.ferraro1@gmail.com>
2020-12-26 06:07:55 +0000

    Automatically backed up by Learn

---
+++ test.rb
@@ -0,0 +1,17 @@
+
+
+def show_array
+  
+  array_of_arrays = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+]
+  count =0
+  while count < array_of_arrays.length do
+  p array_of_arrays[count]
+  count += 1
+end
+end
+
+show_array
